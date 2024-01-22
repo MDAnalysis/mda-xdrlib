903d7b7cacaa881f78895a35129c294f6cb8d1b4
IAlibay <ialibay@mdanalysis.org>
2024-01-22 00:06:03 +0000

    Fix up packaging

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-import versioneer
-
-
-setup(name="mda_xdrlib",
-      include_package_data=True,
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-)
